691baea9410f214d88b8f437a2ef205004a9bc28
Bagas Dhitya <bagasdhityataufiqqi98@gmail.com>
2026-09-07 21:39:55 +0700

    added creatte product function

--- modul-2/day-8/warmad-mart/src/hooks/useProduct.ts
+++ modul-2/day-8/warmad-mart/src/hooks/useProduct.ts
@@ -4,6 +4,12 @@ import { type Product } from "../types/product";
 
 import axios from "axios";
 
+// Omit -> digunakan untuk membuat tipe data baru berdasarkan tipe data yang sudah ada (Type), tetapi menghapus/mengabaikan field tertentu
+export type CreateProductInput = Omit<
+  Product,
+  "objectId" | "created" | "updated"
+>; // tipe data untuk pembuatan produk baru
+
 export function useProducts() {
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
@@ -26,9 +32,33 @@ export function useProducts() {
     }
   }
 
+  // fitur 2 : menambahkan product
+  async function createProduct(newProduct: CreateProductInput) {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.post<Product>(
+        `${BASE_URL}/api/data/products`,
+        newProduct,
+      );
+
+      // update state lokal agar UI langsung merespons
+      setProducts((prev) => [...prev, response.data]);
+      return response.data;
+    } catch (error: any) {
+      setError(error.message || "Gagal menambahkan data produk.");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function updateProduct() {}
+  async function deleteProduct() {}
+
   useEffect(() => {
     getProducts();
   }, []);
 
-  return { products, loading, error };
+  return { products, loading, error, createProduct };
 }
