6385d1739f6219d0389463e67128f5846dcaa289
Bagas Dhitya <bagasdhityataufiqqi98@gmail.com>
2026-09-10 21:41:32 +0700

    added READ, CREATE

--- modul-3/day-1/product-express/src/index.ts
+++ modul-3/day-1/product-express/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Application } from "express";
-import products from "./data/products.json" with { type: "json" };
+import { readProducts, writeProducts } from "./utils/fileHandler.js";
 
 const app: Application = express();
 const PORT: number = 8000;
@@ -17,6 +17,46 @@ app.use(express.json()); // middleware untuk membaca format data JSON
 app.get("/api/hello", (req: Request, res: Response) => {
   return res.status(200).send({
     message: "Hello World",
+  });
+});
+
+// 1. READ ALL - GET /api/products
+app.get("/api/products", (req: Request, res: Response) => {
+  const products = readProducts();
+  return res.status(200).send({
+    data: products,
+    status: res.statusCode,
+  });
+});
+
+// 2. CREATE - POST /api/products
+app.post("/api/products", (req: Request, res: Response) => {
+  const { title, description, stock, price } = req.body;
+
+  // validasi input body request
+  if (!title || !description || stock === undefined || price === undefined) {
+    return res.status(400).send({ message: "All fields are required" });
+  }
+
+  const products: any = readProducts();
+
+  // generate ID baru (auto-increment berdasarkan ID Terakhir)
+  const newId = products.length > 0 ? products[products.length - 1].id + 1 : 1;
+
+  const newProduct: Product = {
+    id: newId,
+    title: title,
+    description: description,
+    stock: stock,
+    price: price,
+  };
+
+  products.push(newProduct);
+  writeProducts(products);
+
+  return res.status(201).send({
+    message: "Product created succefully",
+    status: res.statusCode,
   });
 });
 
